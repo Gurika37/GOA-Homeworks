0d6746f791312f38397a934dc4da2d02c9f14f68
Gurika37 <nikaedisherashvili96@gmail.com>
2024-10-29 00:07:46 +0400

    first commit

---
+++ level06/homework/homework4.py
@@ -0,0 +1,11 @@
+birth_year = "2007"
+birth_month = "05"
+birth_day = "15"
+
+year = int(birth_year)
+month = int(birth_month)
+day = int(birth_day)
+
+print("დაბადების წელი:", year)
+print("დაბადების თვე:", month)
+print("დაბადების დღე:", day)
---
+++ level06/homework/homework5.py
@@ -0,0 +1,6 @@
+Best_Academy = True
+
+GOA_str = str(Best_Academy)
+
+print("boolean ცვლადი:", Best_Academy)
+print("სტრინგ ცვლადი:", GOA_str)
